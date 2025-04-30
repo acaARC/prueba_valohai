4e53f9d98335ba411d82941b4cbbbebaf08c3f2a
Acaymo Granado Sánchez <agranado@arquimea.com>
2025-04-30 12:05:54 +0100

    f

--- valohai_prueba.py
+++ valohai_prueba.py
@@ -4,19 +4,33 @@ import valohai
 from PIL import Image
 import random
 import shutil
+import os
 
 valohai.prepare(step="playing-with-outputs")
 
+carpeta_cualquiera = 'carpeta_cualquiera' # Nombre de la carpeta donde se guardará la imagen
+
+os.makedirs(carpeta_cualquiera, exist_ok=True) # Crear la carpeta si no existe
+
 width, height = 256, 256 # Dimensiones de la imagen
-img = Image.new('RGB', (width, height)) # Crear una nueva imagen RGB
+img1 = Image.new('RGB', (width, height)) # Crear una nueva imagen RGB
 pixels = [
     (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     for _ in range(width * height)
 ] # Asignar a cada píxel un color aleatorio
-img.putdata(pixels) # Colocar los píxeles en la imagen
-output_filename = 'imagen_colores_random.png'
-img.save(output_filename)
+img1.putdata(pixels) # Colocar los píxeles en la imagen
+output_filename = f'{carpeta_cualquiera}/imagen1_colores_random.png'
+img1.save(output_filename)
+
+img2 = Image.new('RGB', (width, height)) # Crear una nueva imagen RGB
+pixels = [
+    (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    for _ in range(width * height)
+] # Asignar a cada píxel un color aleatorio
+img2.putdata(pixels) # Colocar los píxeles en la imagen
+output_filename = f'{carpeta_cualquiera}/imagen2_colores_random.png'
+img2.save(output_filename)
 
 # Definir la ruta de salida en Valohai
-output_path= valohai.outputs().path('mi_ejemplo_output.png')
-shutil.copy(output_filename, output_path) # Copiar la imagen al directorio de salida
+output_path= valohai.outputs().path('mi_ejemplo_output_carpeta')
+shutil.copytree(carpeta_cualquiera, output_path) # Copiar la imagen al directorio de salida
