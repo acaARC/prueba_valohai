099c1d3b93ef4af18deb2001bb7ec7ab282a0ab1
Acaymo Granado Sánchez <agranado@arquimea.com>
2025-04-30 11:25:03 +0100

    f

--- hello.py
+++ hello.py
@@ -6,6 +6,6 @@ valohai.prepare(step="hello-job")
 print("Hello Valohai")
 
 for file in valohai.inputs('dataset').paths():
-    print(f"\n=== Leyendo: {file} ===")
+    print(f"\n=== Leyendo archivo: {file} ===")
     df = pd.read_csv(file)
     print(df.head(5))
