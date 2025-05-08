28a0173ec25a6498a8b60f84072dca99cfd12049
Acaymo Granado Sánchez <agranado@arquimea.com>
2025-05-08 12:31:36 +0100

    f

--- leer_dataset.py
+++ leer_dataset.py
@@ -4,8 +4,8 @@ import os
 # Preparar el entorno
 valohai.prepare(step="leer-dataset", default_inputs={"dataset": "dataset://agranado-dataset/v6"})
 
-# Obtener la ruta al input
-dataset_paths = valohai.inputs('dataset').paths()
+# Obtener la ruta al input y convertir en lista
+dataset_paths = list(valohai.inputs('dataset').paths())
 
 print(f"✅ Dataset cargado: {len(dataset_paths)} archivos encontrados:\n")
 for path in dataset_paths:
