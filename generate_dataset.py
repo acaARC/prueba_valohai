19375807ffe5ef48f2553ca378207aa6f814d689
Acaymo Granado Sánchez <agranado@arquimea.com>
2025-05-08 12:23:54 +0100

    f

--- generate_dataset.py
+++ generate_dataset.py
@@ -11,35 +11,35 @@ os.makedirs(output_dir, exist_ok=True)
 output_files = []
 for i in range(1):
     img = Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8))
-    file_name = f"imagen5_colores_random.png"
+    file_name = f"imagen6_colores_random.png"
     path = os.path.join(output_dir, file_name)
     img.save(path)
     output_files.append(f"mi_ejemplo_output_carpeta/{file_name}")
 
 # Crear metadata del dataset
-metadata = {
+"""metadata = {
     file_name: {
         "valohai.dataset-versions": ["dataset://agranado-dataset/v5"]
     }
     for file_name in output_files
-}
+}"""
 
 
 
 # SI QUEREMOS QUE LA SIGUIENTE VERSION TENGA LO DE LA ANTERIOR + LO NUEVO:
 # Ruta relativa del archivo dentro de outputs
-""" relative_path = f"mi_ejemplo_output_carpeta/{file_name}"
+relative_path = f"mi_ejemplo_output_carpeta/{file_name}"
 
 # Crear metadata con herencia desde v1
 metadata = {
     relative_path: {
         "valohai.dataset-versions": [{
-            'uri': "dataset://agranado-dataset/v4",
-            'from': "dataset://agranado-dataset/v1",
+            'uri': "dataset://agranado-dataset/v6",
+            'from': "dataset://agranado-dataset/v5",
             'start_fresh': False
         }]
     }
-}"""
+}
 
 
 metadata_path = valohai.outputs().path("valohai.metadata.jsonl")
