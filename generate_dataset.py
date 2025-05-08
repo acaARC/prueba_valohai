8fe496f8c636b3eae3a88ced1a43a983af9fb779
Acaymo Granado Sánchez <agranado@arquimea.com>
2025-05-08 10:39:13 +0100

    f

--- generate_dataset.py
+++ generate_dataset.py
@@ -8,24 +8,28 @@ import numpy as np
 output_dir = valohai.outputs().path('mi_ejemplo_output_carpeta')
 os.makedirs(output_dir, exist_ok=True)
 
-output_files = []
-for i in range(1):
-    img = Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8))
-    file_name = f"imagen2_colores_random.png"
-    path = os.path.join(output_dir, file_name)
-    img.save(path)
-    output_files.append(f"mi_ejemplo_output_carpeta/{file_name}")
+# Crear imagen nueva
+file_name = "imagen2_colores_random.png"
+path = os.path.join(output_dir, file_name)
+img = Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8))
+img.save(path)
 
-# Crear metadata del dataset
+# Ruta relativa del archivo dentro de outputs
+relative_path = f"mi_ejemplo_output_carpeta/{file_name}"
+
+# Crear metadata con herencia desde v1
 metadata = {
-    file_name: {
-        "valohai.dataset-versions": ["dataset://agranado-dataset/v2"]
+    relative_path: {
+        "valohai.dataset-versions": [{
+            'uri': "dataset://agranado-dataset/v3",
+            'from': "dataset://agranado-dataset/v1",
+            'start_fresh': False
+        }]
     }
-    for file_name in output_files
 }
 
+# Guardar metadata
 metadata_path = valohai.outputs().path("valohai.metadata.jsonl")
 with open(metadata_path, "w") as f:
-    for file_name, meta in metadata.items():
-        json.dump({"file": file_name, "metadata": meta}, f)
-        f.write("\n")
+    json.dump({"file": relative_path, "metadata": metadata[relative_path]}, f)
+    f.write("\n")
