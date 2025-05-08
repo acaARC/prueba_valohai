563a2094ad5319ab57b6efe7180db586af92b613
Acaymo Granado Sánchez <agranado@arquimea.com>
2025-05-08 10:36:07 +0100

    f

--- generate_dataset.py
+++ generate_dataset.py
@@ -11,7 +11,7 @@ os.makedirs(output_dir, exist_ok=True)
 output_files = []
 for i in range(1):
     img = Image.fromarray(np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8))
-    file_name = f"imagen{i+1}_colores_random.png"
+    file_name = f"imagen2_colores_random.png"
     path = os.path.join(output_dir, file_name)
     img.save(path)
     output_files.append(f"mi_ejemplo_output_carpeta/{file_name}")
@@ -19,7 +19,7 @@ for i in range(1):
 # Crear metadata del dataset
 metadata = {
     file_name: {
-        "valohai.dataset-versions": ["dataset://agranado-dataset/v1"]
+        "valohai.dataset-versions": ["dataset://agranado-dataset/v2"]
     }
     for file_name in output_files
 }
