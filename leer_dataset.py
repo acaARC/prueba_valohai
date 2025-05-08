4c734a8069878d5554203908c0a4b2750e91fa61
Acaymo Granado Sánchez <agranado@arquimea.com>
2025-05-08 12:29:27 +0100

    f

--- leer_dataset.py
+++ leer_dataset.py
@@ -2,7 +2,7 @@ import valohai
 import os
 
 # Preparar el entorno
-valohai.prepare(step="leer-dataset", default_inputs={"dataset": "dataset://agranado-dataset/v4"})
+valohai.prepare(step="leer-dataset", default_inputs={"dataset": "dataset://agranado-dataset/v6"})
 
 # Obtener la ruta al input
 dataset_paths = valohai.inputs('dataset').paths()
